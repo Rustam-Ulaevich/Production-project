670981852a28e0e684adbeab429fa13cc560bcf6
Rustam-Ulaevich <praporglamur@gmail.com>
2025-01-07 13:23:44 +0500

    completed decomposition of the config

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -1,5 +1,3 @@
-
-<<<<<<< HEAD
 export type BuildMode = 'production' | 'development';
 
 export interface BuildPaths {
@@ -13,18 +11,3 @@ export interface BuildOptions {
     paths: BuildPaths;
     isDev: boolean;
 }
-=======
-export type BuildMode = 'production' | 'development'
-
-export interface BuildPaths {
-    entry: string
-    build: string
-    html: string
-}
-
-export interface BuildOptions {
-    mode: BuildMode
-    paths: BuildPaths
-    isDev: boolean
-}
->>>>>>> origin/main
