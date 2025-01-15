112c4235867f8b2f0b589954058fddea4edd09b4
Rustam-Ulaevich <praporglamur@gmail.com>
2025-01-15 18:28:17 +0500

    7.1 Adding themes ThemeContext.ts ThemeProvider.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React, { Suspense } from 'react';
-import './pages/styles/index.scss'
+import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
+import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Theme} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className='app'>
+        <div className={'app ' + theme}>
+            <button onClick={toggleTheme}>{theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT}</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
