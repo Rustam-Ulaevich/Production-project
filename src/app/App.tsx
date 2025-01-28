2423816957182e4f2e1aae95465b99b00d62b453
Rustam-Ulaevich <praporglamur@gmail.com>
2025-01-28 20:54:32 +0500

    10 implemented the FSD architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
 import './styles/index.scss'
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {Theme} from "./theme/ThemeContext";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {AboutPageAsync} from "../pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "../pages/MainPage/MainPage.async";
+import {Theme} from "../shared/config/theme/ThemeContext";
+import {useTheme} from "./providers/ThemeProvider/lib/useTheme";
+import {classNames} from "../helpers/classNames/classNames";
 
 
 
