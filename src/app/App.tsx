95bf40595b6c784145c1727cd8c69207aa43d1bc
Rustam-Ulaevich <praporglamur@gmail.com>
2025-02-06 22:23:26 +0500

    10.2 implemented the FSD architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, {Suspense, useContext, useState} from 'react';
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
 import './styles/index.scss'
-import {AboutPageAsync} from "../pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "../pages/MainPage/MainPage.async";
-import {Theme} from "../shared/config/theme/ThemeContext";
-import {useTheme} from "./providers/ThemeProvider/lib/useTheme";
-import {classNames} from "../helpers/classNames/classNames";
+import {classNames} from "../shared/lib/classNames/classNames";
+import {useTheme} from "./providers/ThemeProvider";
+import AboutPage from "../pages/AboutPage/ui/AboutPage";
+import MainPage from "../pages/MainPage/ui/MainPage";
 
 
 
@@ -14,13 +13,13 @@ const App = () => {
 
     return (
         <div className={classNames('app', {hovered: true}, [theme])}>
-            <button onClick={toggleTheme}>{theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT}</button>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
